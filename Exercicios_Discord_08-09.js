7178302d6a54f6a529c04db3f3211dfa1f0c7c2d
Marcos-sxt <marcossantos7955@gmail.com>
2022-09-08 15:50:44 -0300

    Exercicios_Discord_08-09

---
+++ Exercicios_Discord_08-09.js
@@ -0,0 +1,37 @@
+/*let PC, VV, ACR
+
+PC = prompt("Qual é o preço de custo? ")
+ACR = prompt("Qual é o aumento percentual? ")
+
+VV = (+PC) * (+ACR/100) + (+PC)
+alert("O valor da venda é: " + VV)*/
+
+
+
+
+/*let ValorDepositado
+
+alert("Simulador de rendimentos aos mês")
+ValorDepositado = prompt("Qual o valor depositado? ")
+alert("Depois de um mês você terá: " + ValorDepositado * 1.007)*/
+
+
+
+
+/*let ValorDolar, Cot
+ValorDolar = prompt("digite um valor para ser convertido em dolar: ")
+Cot = prompt("Quanto vale o dolar agora?")
+alert("O valor convertido de Dolar para real fica: " + ValorDolar * Cot + "R$")*/
+
+
+
+
+/*let Numero
+
+Numero = prompt("DIgite a senha: ")
+
+if(Numero > 80 ||Numero < 25 ||Numero == 40){
+    alert("Correto, bem vindo de volta")
+}else{
+    alert("Senha Incorreta!")
+}*/
